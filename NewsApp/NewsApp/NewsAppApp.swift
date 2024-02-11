06c5596719675a144880c0de57c109fefd017f12
Dmitry Tsvetkov <dimacvetkov2000@mail.ru>
2024-02-11 14:15:25 +0300

    Create project

---
+++ NewsApp/NewsApp/NewsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewsAppApp.swift
+//  NewsApp
+//
+//  Created by Дмитрий Цветков on 11.02.2024.
+//
+
+import SwiftUI
+
+@main
+struct NewsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
